afecf25cb4d6e1ed29e8ed6da164652015b0c678
delmas007 <azertyazerty1ze@gmail.com>
2024-05-03 12:16:59 +0000

    chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {RagComponent} from "./rag/rag.component";
+import {ChatComponent} from "./chat/chat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RagComponent],
+  imports: [RouterOutlet, NavbarComponent, RagComponent, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgIf, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgStyle
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent  {
 
+  question: string = '';
+  response: string = '';
+  showResponse: boolean = false;
+
+  @ViewChild('responseTextarea', { static: false }) responseTextarea!: ElementRef;
+
+  isButtonEnabled(): boolean {
+    return this.question.trim().length > 0;
+  }
+
+  submitQuestion(): void {
+    this.response = `Voici la réponse à votre question: ${this.question}`;
+    this.showResponse = true;
+
+    setTimeout(() => {
+      this.adjustTextarea(this.responseTextarea.nativeElement);
+    }, 0); // Un petit délai pour s'assurer que le DOM est à jour
+  }
+
+  adjustTextarea(textarea: HTMLTextAreaElement | Event): void {
+    const textElement: HTMLTextAreaElement = (textarea instanceof Event) ? (<HTMLTextAreaElement>textarea.target) : textarea;
+    textElement.style.height = 'auto'; // Réinitialiser la hauteur
+    textElement.style.height = `${textElement.scrollHeight}px`; // Ajuster la hauteur à celle du contenu
+  }
 }
