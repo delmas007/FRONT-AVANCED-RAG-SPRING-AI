edc4acbd4f4533baf1262128453e1d118894321c
delmas007 <azertyazerty1ze@gmail.com>
2024-05-13 15:45:45 +0000

    security rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import {UserComponent} from "./user/user.component";
 import {RagComponent} from "./rag/rag.component";
 import {RegisterComponent} from "./register/register.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
+import {AuthorizationGuard} from "./guards/authorization.guard";
 
 export const routes: Routes = [
-  { path : "login" , component : LoginComponent},
+
+  { path : "connexion" , component : LoginComponent},
   { path : "inscription" , component : RegisterComponent},
-  { path : "user" , component : UserComponent ,children : [
-      { path : "rag" , component : RagComponent,canActivate:[AuthenticationGuard]},
+
+  { path : "user" , component : UserComponent,canActivate:[AuthenticationGuard] ,children : [
+      { path : "rag" , component : RagComponent,canActivate:[AuthorizationGuard]},
     ]},
+
   { path : "" , redirectTo : "/login", pathMatch : "full"}
 ];
--- src/app/guards/authorization.guard.ts
+++ src/app/guards/authorization.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router, Routes} from '@angular/router';
+import { Observable } from 'rxjs';
+import {StateService} from "../service/state.service";
 
-export const authorizationGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard {
+  constructor(private state:StateService,private router : Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.state.authState.role == "ADMIN" || this.state.authState.role == "USER") {
+      return true;
+    } else {
+      this.router.navigateByUrl("/admin/notAuthorized");
+      return false;
+    }
+  }
+}
