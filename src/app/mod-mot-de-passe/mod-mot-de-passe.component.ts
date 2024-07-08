a852ddbc227615f4673806a2b63316a27d26b936
delmas007 <azertyazerty1ze@gmail.com>
2024-07-08 11:27:05 +0000

    gestion mot de passe

---
+++ src/app/mod-mot-de-passe/mod-mot-de-passe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mod-mot-de-passe',
+  standalone: true,
+  imports: [],
+  templateUrl: './mod-mot-de-passe.component.html',
+  styleUrl: './mod-mot-de-passe.component.css'
+})
+export class ModMotDePasseComponent {
+
+}
