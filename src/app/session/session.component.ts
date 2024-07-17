4f7d99d552609ede23719af6122b61fb20072c64
delmas007 <azertyazerty1ze@gmail.com>
2024-07-17 13:36:04 +0000

    session et authorisation

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-session',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './session.component.html',
   styleUrl: './session.component.css'
 })
